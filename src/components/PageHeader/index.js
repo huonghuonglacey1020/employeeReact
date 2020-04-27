c19177b73515958558a44c4ab5871ef27ca2f9aa
huongnguyen <huonghuonglacey1020@gmail.com>
2020-04-27 16:08:11 -0700

    react

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -7,7 +7,7 @@ export default class PageHeader extends React.Component {
             <div>
             <header>
 
-                <nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar text-center text-light bg-dark">
+                <nav className="App-header navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar text-center text-light bg-dark">
                     <strong>Employee Directory</strong>
                     
             </nav>
