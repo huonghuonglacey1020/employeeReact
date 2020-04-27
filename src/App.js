eeb7cc78174af830b98de88595c7cdf53f246583
huongnguyen <huonghuonglacey1020@gmail.com>
2020-04-27 11:21:11 -0700

    react file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeSearch from './components/EmployeeSearch'
+
+function App() {
+  return (
+    <div>
+<EmployeeSearch></EmployeeSearch>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class PageHeader extends React.Component {
+
+    render() {
+        return (
+            <div>
+            <header>
+
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar text-center text-light bg-dark">
+                    <strong>Employee Directory</strong>
+                    
+            </nav>
+        </header>
+    </div>
+            
+        )
+    }
+}
