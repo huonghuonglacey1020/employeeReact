caacaf65456753d68566b5956fa29ac8839ab1a9
huongnguyen <huonghuonglacey1020@gmail.com>
2020-05-12 23:39:04 -0700

    Updates

---
+++ precache-manifest.c6a87b43fef46aaef83f903b7244eba4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873985639c1c489463f4e3895aeca93f",
+    "url": "/huonghuonglacey1020/employeeReact.git/index.html"
+  },
+  {
+    "revision": "d0e7cb205bfb58100091",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/css/2.b31a0095.chunk.css"
+  },
+  {
+    "revision": "727550ef10dcebe24213",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0e7cb205bfb58100091",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/js/2.55992e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/js/2.55992e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727550ef10dcebe24213",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/js/main.f1e0a472.chunk.js"
+  },
+  {
+    "revision": "765069a0b7352106e6ce",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/js/runtime-main.2ac40e4b.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/huonghuonglacey1020/employeeReact.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/huonghuonglacey1020/employeeReact.git/precache-manifest.c6a87b43fef46aaef83f903b7244eba4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/huonghuonglacey1020/employeeReact.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
